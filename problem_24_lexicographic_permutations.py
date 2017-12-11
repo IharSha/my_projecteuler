e337393fd5499a05f75af075cdd93a0c7164018a
Ihar <qrutas@gmail.com>
2017-12-11 13:27:47 +0300

    wise use of permutation generator in problem 24

--- problem_24_lexicographic_permutations.py
+++ problem_24_lexicographic_permutations.py
@@ -9,7 +9,14 @@ import itertools
 
 What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?"""
 
-digits = list('0123456789')
+millionth_permutation = ''
 
-permutation_list = list(itertools.permutations(digits))
-print(''.join(permutation_list[1000000-1]))
+permutation_list = itertools.permutations(list('0123456789'))
+counter = 0
+for i in permutation_list:
+    counter += 1
+    if counter == 1000000:
+        millionth_permutation = i
+        break
+
+print(''.join(millionth_permutation))
