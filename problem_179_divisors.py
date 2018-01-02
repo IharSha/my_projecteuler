667d89cded7aa4dade6e5cad264df94dd723fa6c
Ihar <qrutas@gmail.com>
2018-01-02 11:57:27 +0300

    problem 179 solved;

--- problem_179_divisors.py
+++ problem_179_divisors.py
@@ -1,23 +1,16 @@
-def total_divisors(n):
-    count = 2  # accounts for 'n' and '1'
-    i = 2
-    while i**2 < n:
-        if n % i == 0:
-            count += 2
-        i += 1
-    count += (1 if i**2 == n else 0)
-    return count
+limit = 10**7
+table = [1 for i in range(limit + 1)]
 
+for i in range(1, limit + 1):
+    N = i
+    while N < limit:
+        table[N] += 1
+        N += i
 
-ans = 0
-prv_total_divisors = 0
+C = 0
 
-for i in range(2, 10000000):
-    if i % 200000 == 0:
-        print(i)
-    total_divs = total_divisors(i)
-    if total_divs == prv_total_divisors:
-        ans += 1
-    prv_total_divisors = total_divs
+for n in range(1, limit + 1):
+    if table[n-1] == table[n]:
+        C += 1
 
-print(ans)
+print(C)
